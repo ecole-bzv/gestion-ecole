80cdf5de790258bf240a5f03bfe7a6aa607f2f5e
ecole-bzv <widemorningstar@gmail.com>
2026-08-11 02:55:25 +0200

    Refactor server.js by removing database connection
    Removed PostgreSQL pool connection and related queries. Updated the response for the '/api/etudiants' endpoint.

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const {Pool} = require ('pg');
 const cors = require ('cors');
 
 const app = express();
@@ -7,30 +6,8 @@ app.use ( cors()) ;
 app.use (express.json());
 app.use(express.static( 'public'));
 
-const poo = new Pool({ 
-  connectionStrin: process.env.DATABASE_URL,
-  ssl: rejectUnauthorized: false}
-});
-
-pool.query REATE TABLE IF NOT EXISTS etudiants ( 
-  id SERIAL PRIMARY KEY,
-  nom VARCHAR (100),
-  prnom VARCHAR (100),
-  created_at TIMESTAMP DEFAULT NOW()
-)`);
-
 app.get('/api/etudiants' , async (req, res) => 
-  const result = await pool.quey ('SELECT *FROM etudiants ORDER BY id DESC');
-  res.json result.rows);
-});
-
-app.get('/api/etudiants' , async (req, res) =>
-  const nom, prenom = req.body;
-  const result = await pool.quey (
-  'INNER INTO etudiants(nom, prenom) VALUES ($1 , $2) RETURNING *',
-  [nom, prenom]
-);
-res.json(result.row[0]);
+  res.json({message: "serveur OK!" });
 });
 
 const PORT = process.env.PORT || 10000;
